809c7d523144ec0eec107f87691ab09d5cf41568
Reinhold Willcox <reinhold.willcox@gmail.com>
2025-01-14 23:35:28 +0100

    fixed priors

--- src/Priors.jl
+++ src/Priors.jl
@@ -13,8 +13,8 @@ Priors contains the prior distribution of each of the desired parameters
     vkick_dist::Union{ContinuousUnivariateDistribution,Missing} = missing
     frac_dist::Union{ContinuousUnivariateDistribution,Missing} = missing 
     e_dist::Union{ContinuousUnivariateDistribution,Missing} = missing       
-    venv_N_100kms_dist::Union{ContinuousUnivariateDistribution,Missing} = missing       
-    venv_E_100kms_dist::Union{ContinuousUnivariateDistribution,Missing} = missing       
+    venv_δ_100kms_dist::Union{ContinuousUnivariateDistribution,Missing} = missing       
+    venv_α_100kms_dist::Union{ContinuousUnivariateDistribution,Missing} = missing       
     venv_r_100kms_dist::Union{ContinuousUnivariateDistribution,Missing} = missing       
     rv_env_dist::Union{ContinuousUnivariateDistribution,Missing} = missing 
     pmra_dist::Union{ContinuousUnivariateDistribution,Missing} = missing  
